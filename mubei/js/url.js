25c7e7bde7df40100e77d260c44f0ff492c8a3f6
arvin <arvin.zhang@ve-link.com>
2018-06-18 12:18:49 +0800

    修改mysql数据库账号密码

--- mubei/js/url.js
+++ mubei/js/url.js
@@ -1,10 +1,10 @@
 var url = new Array();
-var j_url = url[0] = "http://www.malaxyb.com/new_vpay_api/public/index.php/index\/";/*120.78.193.17*/
-var img_url = url[1] = "http://www.malaxyb.com/new_vpay_api/public/static/uploads/";/*192.168.5.26*/
+var j_url = url[0] = "http://www.malaxyb.io/new_vpay_api/public/index.php/index\/";/*120.78.193.17*/
+var img_url = url[1] = "http://www.malaxyb.io/new_vpay_api/public/static/uploads/";/*192.168.5.26*/
 //var j_url = url[0] = "http://192.168.0.177/cmb/new_vpay_api/public/index/";/*120.78.193.17*/
 //var img_url = url[1] = "http://192.168.0.177/cmb/new_vpay_api/public/static/uploads/";/*192.168.5.26*/
-var p_url = url[2] = "http://www.malaxyb.com/mall/public/index.php/api/api/";
-var i_url = url[3]="http://www.malaxyb.com/"
+var p_url = url[2] = "http://www.malaxyb.io/mall/public/index.php/api/api/";
+var i_url = url[3]="http://www.malaxyb.io/"
 /*去掉滚动条*/
 //mui.plusReady(function() {
 //		plus.webview.currentWebview().setStyle({scrollIndicator:'none'});
